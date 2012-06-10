8efacece2a67b5e660ac37430c725bc57e57cf3d
Gabriel Horner <gabriel.horner@gmail.com>
2012-06-10 03:07:12 -0400

    reorganize render-rows, make header more obvious

--- src/table/core.clj
+++ src/table/core.clj
@@ -1,22 +1,27 @@
 (ns table.core
   (:use [clojure.string :only [join]] ))
 
-(defn render [table]
-  (let [spacer
-         (str "+-"
-           (join "-+-"
-             (map #(apply str (repeat (.length %) "-"))
-               (first table)))
-                    "-+")]
-    (concat [spacer
-             (str "| " (join " | " (first table)) " |")
-             spacer]
-            (for [tr (rest table)]
-              (str "| " (join " | " tr) " |"))
-            [spacer])))
+(defn render-rows [table]
+  (let [
+    border
+     (str "+-"
+       (join "-+-"
+         (map #(apply str (repeat (.length %) "-"))
+           (first table)))
+                "-+")
+    header [
+      border
+      (str "| " (join " | " (first table)) " |")]]
+
+    (concat
+      header
+      [border]
+      (for [tr (rest table)]
+        (str "| " (join " | " tr) " |"))
+      [border])))
 
 (defn table-str [& args]
-  (apply str (join "\n" (apply render args))))
+  (apply str (join "\n" (apply render-rows args))))
 
 (defn table [& args]
   (print (apply table-str args)))
