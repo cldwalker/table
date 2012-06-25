6af9cb5801cd8f03448e7469212a20ecf88af3b8
Gabriel Horner <gabriel.horner@gmail.com>
2012-06-25 00:04:02 -0400

    update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject table "0.1.0-SNAPSHOT"
-  :description "Simple to use ascii tables"
+(defproject table "0.1.0"
+  :description "Display ascii tables for almost any data structure with ease"
   :url "http://github.com/cldwalker/table"
   :license {:name "The MIT License"
             :url "https://en.wikipedia.org/wiki/MIT_License"}
   :main table.core
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
