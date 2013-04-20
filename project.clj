eeada9793e1b087235d97e2acfee05c1e98e6a7d
Gabriel Horner <gabriel.horner@gmail.com>
2013-04-20 17:01:21 -0400

    depend on 1.5

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "The MIT License"
             :url "https://en.wikipedia.org/wiki/MIT_License"}
   :repl-options  {:init-ns table.core}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.5"]})
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles  {:1.4  {:dependencies  [[org.clojure/clojure "1.4.0"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.4"]})
